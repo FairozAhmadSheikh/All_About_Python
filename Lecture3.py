d2ce2bbc272cbecccbb00ae051a6771ceca840f3
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-10-04 18:48:15 +0530

    Madlibs_Game

---
+++ Lecture3.py
@@ -0,0 +1,13 @@
+# Madlibs Game 
+# This actually is a fill in the balnks game 
+adjective1=input("Enter Adjective (Description) : ") or "Expensive"
+noun1=input("Enter a Noun (place,person or thing) : ") or "Gorilla"
+adjective2=input("Enter Adjective Second : ") or "Angry"
+verb1=input("Enter a verb ending with 'ing' : ") or "Screeching"
+adjective3=input("Enter Thrid Adjective : ")  or "Amazed"
+print()
+print("Here Starts Output! ".center(30,"="))
+print(f"Today i went to {adjective1} zoo  ")
+print(f"In an Exhibit, I saw {noun1} ")
+print(f"{noun1} was very {adjective2} and {verb1}")
+print(f"I was {adjective3}")
