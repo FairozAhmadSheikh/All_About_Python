8b85d880fafb76de966a5b2b8a4df4b451e58164
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-10-04 18:31:44 +0530

    Initalization

---
+++ Lecture1.py
@@ -0,0 +1,9 @@
+# Exercise One 
+# Rectangle Area
+# User Inputs
+length= float(input("Enter Length of Rectangle : \n"))
+width=float(input("Enter Width of Rectangle : \n"))
+# Logic 
+area=length*width
+# Output
+print(f"Area of Rectangle is : {area} cm^2")
