85d12aafea1f75e5ba7f0d7788e4c1b14d0f4bba
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-10-04 19:20:02 +0530

    ✅ Arithamtic and maths

---
+++ Easy/Lecture4.py
@@ -0,0 +1,43 @@
+import math 
+x=3.12
+# Round rounds to nearest whole integer
+print(round(x))
+
+#Absolute value for negitive numbers 
+y=-4
+print(abs(y))
+
+# Power Function is used to find y powered to x it takes two parameters base and power
+print(pow(4,2))
+
+# Maximum Value Finder  max() finds maximum value 
+a=10
+b=20
+c=30
+maximum=max(a,b,c)
+print("maximum is ",maximum)
+# Maximum Value Finder  max() finds maximum value 
+a=10
+b=20
+c=30
+minimum=min(a,b,c)
+print("minimum is ",minimum)
+
+
+# A bit advanced use of math by importing math library
+
+# Square Root
+print(math.sqrt(72))
+
+# Pi value 3.14
+print(math.pi)
+
+# e value for physics Exponential Constant
+print(math.e) 
+
+# Ceil rounds number to Upper value
+q=9.1
+print(math.ceil(q))
+# Floor rounds number to lower value
+q=9.9
+print(math.floor(q))
